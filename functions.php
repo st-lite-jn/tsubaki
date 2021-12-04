fdb1b4663595358a26c0dc69297d98c49785e97d
jey <diolite.jn@gmail.com>
2021-12-04 19:14:49 +0900

    Version 0.9.10.1
    - 使用しないファンクション定義ファイルを削除

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 $tsbk_funcs_globs = array_merge(
-	glob(__DIR__ . '/functions/snippets/*.php'), // テーマ内で使用するスニペット関数
-	glob(__DIR__ . '/functions/settings/*.php'), // テーマの設定に関する関数
-	glob(__DIR__ . '/functions/admin-screens/*.php'), // WordPressの管理画面に関する関数
-	glob(__DIR__ . '/functions/global-variables/*.php'), // テーマ内のグローバル変数を返す関数
-	glob(__DIR__ . '/functions/outputs/*.php'),// テンプレート内のHTMLを出力する
-	glob(__DIR__ . '/functions/theme-customizers/*.php'),// テーマカスタマイザーの定義
-	glob(__DIR__ . '/functions/block-styles/*.php'), //
+	glob(__DIR__ . '/functions/snippets/*.php'),
+	glob(__DIR__ . '/functions/settings/*.php'),
+	glob(__DIR__ . '/functions/admin-screens/*.php'),
+	glob(__DIR__ . '/functions/global-variables/*.php'),
+	glob(__DIR__ . '/functions/outputs/*.php'),
+	glob(__DIR__ . '/functions/theme-customizers/*.php'),
+	glob(__DIR__ . '/functions/block-styles/*.php'),
 );
 foreach($tsbk_funcs_globs as $tsbk_funcs_glob) {
 	include $tsbk_funcs_glob;
