685bd808f4154911f6cec4278deed813906dc56e
jey <diolite.jn@gmail.com>
2021-12-18 21:30:31 +0900

    Version  0.9.11.3
    - wp-block-libraryをインライン化

--- functions/settings/_styles.php
+++ functions/settings/_styles.php
@@ -9,6 +9,14 @@ function tsbk_enqueue_styles() {
 	wp_enqueue_style( 'tsbk-googlefonts' , 'https://fonts.googleapis.com/icon?family=Material+Icons%7CMaterial+Icons+Outlined&display=swap' );
 
 	/**
+	 * wp-block-libraryを登録
+	 */
+	wp_deregister_style( 'wp-block-library' );
+	wp_register_style( 'wp-block-library' , false);
+	wp_enqueue_style( 'wp-block-library');
+	wp_add_inline_style( 'wp-block-library' , file_get_contents( ABSPATH .'wp-includes/css/dist/block-library/style.min.css'));
+
+	/**
 	 * style.cssを登録
 	 */
 	wp_enqueue_style( 'tsbk-style' , get_theme_file_uri()."/assets/css/style.css" );
