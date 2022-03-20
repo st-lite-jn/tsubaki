f4033afa8809ecf07b8edcaf78de91dc7fd301ee
jey <diolite.jn@gmail.com>
2022-03-21 01:08:19 +0900

    Version 0.9.18 - グループ化した場合のレイアウトを実装 - 見出しブロックのブロックパターンを修正

--- functions/block-styles/heading.php
+++ functions/block-styles/heading.php
@@ -3,7 +3,18 @@ register_block_style(
 	'core/heading',
 	array(
 		'name'			=> 'center',
-		'label'			=> '真ん中寄せ',
+		'label'			=> __( '中央寄せ', 'tsubaki' ),
 		'style_handle'	=> 'editor',
 	)
+
+);
+
+register_block_style(
+	'core/heading',
+	array(
+		'name'			=> 'border',
+		'label'			=> __( 'ボーダーライン', 'tsubaki' ),
+		'style_handle'	=> 'editor',
+	)
+
 );
