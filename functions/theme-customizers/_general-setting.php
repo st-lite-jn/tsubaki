fc91e28a5353def45f2ecbaa5155b94e9773ee72
jey <diolite.jn@gmail.com>
2022-05-08 13:23:09 +0900

    Version 1.0 - テーマカスタマイザーにカラー設定の項目を実装 - WordPressデフォルトのフォントサイズを書き換え

--- functions/theme-customizers/_general-setting.php
+++ functions/theme-customizers/_general-setting.php
@@ -64,25 +64,196 @@ if(!function_exists('tsbk_tc_general')) {
 		$wp_customize -> add_setting(
 			'tsbk_setting_color-base-font',
 			array(
-			  'default'           => '',
-			  'priority'          => 1100,
-			  'sanitize_callback' => 'sanitize_hex_color'
+				'default'           => '',
+				'priority'          => 1100,
+				'sanitize_callback' => 'sanitize_hex_color'
 			)
-		  );
-		  $wp_customize -> add_control(
+		);
+		$wp_customize -> add_control(
 			new WP_Customize_Color_Control(
-			  $wp_customize,
-			  'tsbk_control_color-base-font',
-			  array(
+				$wp_customize,
+				'tsbk_control_color-base-font',
+				array(
 				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
 				'settings'    => 'tsbk_setting_color-base-font',  // 紐づける設定IDを指定
 				'label'       => 'ベースフォントカラー',
 				'description' => ''
-			  )
+				)
 			)
 		);
 
 
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-main',
+			array(
+			  'default'           => '',
+			  'priority'          => 1000,
+			  'sanitize_callback' => 'sanitize_hex_color'
+			)
+		  );
+		  $wp_customize->add_control(
+			new WP_Customize_Color_Control(
+			  $wp_customize,
+			  'tsbk_control_color-main',
+			  array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-main',  // 紐づける設定IDを指定
+				'label'       => 'メインカラー',
+				'description' => ''
+			  )
+			)
+		);
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-main-font',
+			array(
+				'default'           => '',
+				'priority'          => 1100,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+		);
+
+		$wp_customize -> add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-main-font',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-main-font',  // 紐づける設定IDを指定
+				'label'       => 'メインフォントカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-acc',
+			array(
+				'default'           => '',
+				'priority'          => 1000,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+			);
+			$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-acc',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-acc',  // 紐づける設定IDを指定
+				'label'       => 'アクセントカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-acc-font',
+			array(
+				'default'           => '',
+				'priority'          => 1100,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+		);
+		$wp_customize -> add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-acc-font',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-acc-font',  // 紐づける設定IDを指定
+				'label'       => 'アクセントフォントカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-sub',
+			array(
+				'default'           => '',
+				'priority'          => 1000,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+			);
+			$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-sub',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-sub',  // 紐づける設定IDを指定
+				'label'       => 'サブカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-sub-font',
+			array(
+				'default'           => '',
+				'priority'          => 1100,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+		);
+		$wp_customize -> add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-sub-font',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-sub-font',  // 紐づける設定IDを指定
+				'label'       => 'サブフォントカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-link',
+			array(
+				'default'           => '',
+				'priority'          => 1000,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+		);
+		$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-link',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-link',  // 紐づける設定IDを指定
+				'label'       => 'リンクカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		$wp_customize -> add_setting(
+			'tsbk_setting_color-border',
+			array(
+				'default'           => '',
+				'priority'          => 1000,
+				'sanitize_callback' => 'sanitize_hex_color'
+			)
+		);
+		$wp_customize->add_control(
+			new WP_Customize_Color_Control(
+				$wp_customize,
+				'tsbk_control_color-border',
+				array(
+				'section'     => 'tsbk_section_color',  // 紐づけるセクションIDを指定
+				'settings'    => 'tsbk_setting_color-border',  // 紐づける設定IDを指定
+				'label'       => 'ボーダーカラー',
+				'description' => ''
+				)
+			)
+		);
+
+		/**
+		 * テキスト設定
+		 */
 		$wp_customize -> add_section (
 		'tsbk_section_txt',
 			array(
@@ -92,7 +263,7 @@ if(!function_exists('tsbk_tc_general')) {
 			)
 		);
 		$wp_customize->add_setting(
-			'tsbk_seeting_copyright', // 設定ID
+			'tsbk_setting_copyright', // 設定ID
 			array(
 				'default' => '',
 				'priority' => 1000,
@@ -103,7 +274,7 @@ if(!function_exists('tsbk_tc_general')) {
 			'tsbk_control_copyright',
 			array(
 			  'section'     => 'tsbk_section_txt',  // 紐づけるセクションIDを指定
-			  'settings'    => 'tsbk_seeting_copyright',  // 紐づける設定IDを指定
+			  'settings'    => 'tsbk_setting_copyright',  // 紐づける設定IDを指定
 			  'label'       => 'コピーライト',
 			  'description' => 'コピーライトを入力',
 			  'type'        => 'text'
@@ -114,5 +285,74 @@ if(!function_exists('tsbk_tc_general')) {
 add_action( 'customize_register', 'tsbk_tc_general' );
 
 
+/**
+ * 一般設定のカラー設定を格納する関数
+ */
+if( !function_exists( 'tsbk_tc_custom_color_style' ) ):
+function tsbk_tc_custom_color_style( ) {
+	$cutom_color_style = false;
+	if(
+		!empty( get_theme_mod( 'tsbk_setting_color-base' ) )
+		|| !empty( get_theme_mod( 'tsbk_control_color-base-font' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-main' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-main-font' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-acc' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-acc-font' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-sub' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-sub-font' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-link' ) )
+		|| !empty( get_theme_mod( 'tsbk_setting_color-border' ) )
+	):
+		$colors = [
+			"base" => get_theme_mod( 'tsbk_setting_color-base' ),
+			"base-font" => get_theme_mod( 'tsbk_setting_color-base-font' ),
+			"main" => get_theme_mod( 'tsbk_setting_color-main' ),
+			"main-font" =>  get_theme_mod( 'tsbk_setting_color-main-font' ),
+			"acc" => get_theme_mod( 'tsbk_setting_color-acc' ),
+			"acc-font" => get_theme_mod( 'tsbk_setting_color-acc-font' ),
+			"sub" => get_theme_mod( 'tsbk_setting_color-acc' ),
+			"sub-font" => get_theme_mod( 'tsbk_setting_color-acc-font' ),
+			"link" =>  get_theme_mod( 'tsbk_setting_color-link' ),
+			"border" => get_theme_mod( 'tsbk_setting_color-border' )
+		];
+
+		$cutom_color_style .= "<style>:root{";
+		if( !empty( $colors['base'] ) ){
+			$cutom_color_style .= "	--tbk-color-base : {$colors['base']} ;";
+		}
+		if( !empty( $colors['base-font'] ) ) {
+			$cutom_color_style .= "	--tbk-color-base-font : {$colors['base-font']} ;";
+		}
+		if( !empty( $colors['main'] ) ) {
+			$cutom_color_style .= "	--tbk-color-main : {$colors['main']} ;";
+		}
+		if( !empty( $colors['main-font'] ) ) {
+			$cutom_color_style .= "	--tbk-color-main-font : {$colors['main-font']} ;";
+		}
+		if( !empty( $colors['acc'] ) ) {
+			$cutom_color_style .= "	--tbk-color-acc : {$colors['acc']} ;";
+		}
+		if( !empty( $colors['acc-font'] ) ) {
+			$cutom_color_style .= "	--tbk-color-acc-font : {$colors['acc-font']} ;";
+		}
+		if( !empty( $colors['sub'] ) ) {
+			$cutom_color_style .= "	--tbk-color-sub : {$colors['sub']} ;";
+		}
+		if( !empty( $colors['sub-font'] ) ) {
+			$cutom_color_style .= "	--tbk-color-sub-font : {$colors['sub-font']} ;";
+		}
+		if( !empty( $colors['link'] ) ) {
+			$cutom_color_style .= "	--tbk-color-link : {$colors['link']} ;";
+		}
+		if( !empty( $colors['border'] ) ) {
+			$cutom_color_style .= "	--tbk-color-border : {$colors['border']} ;";
+		}
+		$cutom_color_style .= "}</style>";
+	endif;
+	echo $cutom_color_style;
+}
+endif;
+add_action( 'wp_head', 'tsbk_tc_custom_color_style' );
+add_action( 'admin_head', 'tsbk_tc_custom_color_style' );
 
 
--- template-parts/footer/copyright.php
+++ template-parts/footer/copyright.php
@@ -1,3 +1,3 @@
-<?php if(get_theme_mod( 'tsbk_copyright' )):?>
-<p class="l-footer__copyright"><?php echo get_theme_mod( 'tsbk_copyright' );?></p>
+<?php if(get_theme_mod( 'tsbk_setting_copyright' )):?>
+<p class="l-footer__copyright"><?php echo get_theme_mod( 'tsbk_setting_copyright' );?></p>
 <?php endif;?>
