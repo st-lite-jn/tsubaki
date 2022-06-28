7267d9902e19d1a93af436f9a46572487fd83d59
jey <diolite.jn@gmail.com>
2022-06-28 22:16:02 +0900

    Version 1.3 - create custom blockを削除

--- functions/admin/_post-screen.php
+++ functions/admin/_post-screen.php
@@ -8,8 +8,9 @@
 function tsbk_enqueue_block_editor_style() {
 	// ブロックエディタ用スタイル機能をテーマに追加
 	add_theme_support( 'editor-styles' );
+
 	add_editor_style( [
-		'//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap',
+		'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap',
 		'assets/css/editor.css'
 	]);
 };
