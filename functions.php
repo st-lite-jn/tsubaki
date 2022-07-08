7a086198117536c7039ad0de2ee29e0e293c8375
jey <diolite.jn@gmail.com>
2022-07-08 22:53:25 +0900

    Version 1.3 - カスタムブロックエディタのテンプレート開発

--- functions.php
+++ functions.php
@@ -6,7 +6,6 @@ $tsbk_funcs_globs = array_merge(
 	glob(__DIR__ . '/functions/global-variables/*.php'),
 	glob(__DIR__ . '/functions/outputs/*.php'),
 	glob(__DIR__ . '/functions/theme-customizers/*.php'),
-	glob(__DIR__ . '/functions/register-custom-blocks/*.php'),
 	glob(__DIR__ . '/functions/block-patterns/*.php'),
 	glob(__DIR__ . '/functions/resister-block-styles/*.php'),
 	glob(__DIR__ . '/functions/unresister-block-styles/*.php'),
@@ -16,3 +15,5 @@ foreach($tsbk_funcs_globs as $tsbk_funcs_glob) {
 }
 
 include __DIR__ .  "/custom-blocks/custom-blocks.php";
+
+include __DIR__ .  "/tsbk-block/tsbkblock.php";
--- functions/settings/_wp-head.php
+++ functions/settings/_wp-head.php
@@ -6,7 +6,7 @@
 /*
 * 絵文字のDNS Prefetchを削除
 */
-add_filter( 'emoji_svg_url', '__return_false');
+//add_filter( 'emoji_svg_url', '__return_false');
 
 /*
 * 不要なhead要素を削除
@@ -17,7 +17,7 @@ remove_action('wp_head', 'wlwmanifest_link');// ブログエディターのマ�
 remove_action('wp_head', 'rsd_link');// 外部から編集するためのAPI（XML-RPC)へのパス
 remove_action('wp_head', 'feed_links_extra', 3);// フィードへのリンク
 remove_action('wp_head', 'print_emoji_detection_script', 7);// 絵文字に関するJavaScript
-remove_action('wp_head', 'rest_output_link_wp_head'); //REST API のパス
+// remove_action('wp_head', 'rest_output_link_wp_head'); //REST API のパス
 // remove_action('wp_head', 'wp_oembed_add_discovery_links');//引用表示（埋め込み）関連のコード
 // remove_action('wp_head','wp_oembed_add_host_js');
 remove_action('wp_print_styles', 'print_emoji_styles');// 絵文字に関するCSS
