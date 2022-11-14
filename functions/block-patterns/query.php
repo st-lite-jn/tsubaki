467ba3595eaae6c27e325a95714727a998b8bca0
jey <diolite.jn@gmail.com>
2022-11-14 10:37:49 +0900

    v1.2.3.3
    - クエリブロックを修正

--- functions/block-patterns/query.php
+++ functions/block-patterns/query.php
@@ -8,38 +8,44 @@ function tsbk_rbp_query() {
 			"categories" => array('query'),
 			'description' => 'サムネイル画像付きのクエリーループ',
 			'content' => '
-                <!-- wp:query {"queryId":1,"query":{"perPage":"4","pages":0,"offset":0,"postType":"post","categoryIds":[],"tagIds":[],"order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"list","columns":3}} -->
-                <div class="wp-block-query"><!-- wp:post-template {"className":"is-custom-block-query"} -->
-                <!-- wp:columns {"align":"wide"} -->
-                <div class="wp-block-columns alignwide"><!-- wp:column {"width":"25%","className":"is-thumbnail"} -->
-                <div class="wp-block-column is-thumbnail" style="flex-basis:25%"><!-- wp:post-featured-image {"isLink":true} /--></div>
-                <!-- /wp:column -->
+			<!-- wp:query {"queryId":1,"query":{"perPage":"4","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"list","columns":3},"className":"wp-tsbk-query"} -->
+<div class="wp-block-query wp-tsbk-query"><!-- wp:post-template -->
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"25%","className":"is-thumbnail"} -->
+<div class="wp-block-column is-thumbnail" style="flex-basis:25%"><!-- wp:post-featured-image {"isLink":true} /--></div>
+<!-- /wp:column -->
 
-                <!-- wp:column {"width":"75%"} -->
-                <div class="wp-block-column" style="flex-basis:75%"><!-- wp:group -->
-                <div class="wp-block-group"><!-- wp:post-date /-->
+<!-- wp:column {"width":"75%"} -->
+<div class="wp-block-column" style="flex-basis:75%"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:post-date /-->
 
-                <!-- wp:post-title {"isLink":true} /-->
+<!-- wp:post-title {"isLink":true} /-->
 
-                <!-- wp:post-excerpt {"moreText":"続きを読む"} /-->
+<!-- wp:post-excerpt {"moreText":"続きを読む"} /-->
 
-                <!-- wp:post-terms {"term":"category","className":"is-cat"} /-->
+<!-- wp:post-terms {"term":"category","className":"is-cat"} /-->
 
-                <!-- wp:post-terms {"term":"post_tag","className":"is-tag"} /--></div>
-                <!-- /wp:group --></div>
-                <!-- /wp:column --></div>
-                <!-- /wp:columns -->
-                <!-- /wp:post-template -->
+<!-- wp:post-terms {"term":"post_tag","className":"is-tag"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:post-template -->
 
-                <!-- wp:query-pagination {"paginationArrow":"chevron","layout":{"type":"flex","justifyContent":"center","flexWrap":"nowrap"}} -->
-                <!-- wp:query-pagination-previous /-->
+<!-- wp:query-pagination {"paginationArrow":"chevron","layout":{"type":"flex","justifyContent":"center","flexWrap":"nowrap"}} -->
+<!-- wp:query-pagination-previous /-->
 
-                <!-- wp:query-pagination-numbers /-->
+<!-- wp:query-pagination-numbers /-->
 
-                <!-- wp:query-pagination-next /-->
-                <!-- /wp:query-pagination --></div>
-                <!-- /wp:query -->
-            '
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"クエリが結果を返さない場合に表示するテキスト、またはブロックを追加してください。"} -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query -->
+'
 		)
 	);
 }
--- template-parts/archive/loop.php
+++ template-parts/archive/loop.php
@@ -4,15 +4,19 @@
 	* $the_query : WP_Queryオブジェクト
 	*/
 ?>
-<div class="wp-block-query">
-	<ul class="wp-block-post-template">
+<div class="is-layout-flow wp-block-query">
+	<ul class="is-layout-flow is-custom-block-query wp-block-post-template">
 <?php while ( $the_query -> have_posts()) : $the_query -> the_post(); ?>
 <?php
 	//サムネイル画像を取得
-	$thumbnail = get_post_thumbnail_id( get_the_ID()) ? wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ) , 'medium', false , array('class'=>'')) : false;
-
+	$thumbnail = get_post_thumbnail_id( get_the_ID())
+			   ? wp_get_attachment_image( get_post_thumbnail_id( get_the_ID() ) , 'medium', false , array('class'=>'attachment-post-thumbnail size-post-thumbnail wp-post-image'))
+			   : false;
+	$parmalink = get_the_permalink( get_the_ID() );
 	if($thumbnail) {
-		$thumbnail = '<figure class="wp-block-post-featured-image"><a href="' . get_the_permalink( get_the_ID() ) .'">'.$thumbnail.'</a></figure>';
+		$thumbnail = "<figure class=\"wp-block-post-featured-image\">
+						<a href\"{$parmalink}\">{$thumbnail}</a>
+					  </figure>";
 	}
 
 	//POST IDからタクソノミーを取得
@@ -20,37 +24,40 @@
 ?>
 <li class="wp-block-post">
 	<div class="wp-block-columns alignwide">
-		<div class="wp-block-column is-thumbnail" style="flex-basis:25%"><?php echo $thumbnail;?></div>
-		<div class="wp-block-column" style="flex-basis:75%">
-			<div class="wp-block-group">
-				<?php if(!is_search(  )):?>
-				<div class="wp-block-post-date">
-					<time datetime="<?php echo get_the_time("Y-m-d"); ?>"><?php echo get_the_time("Y.m.d"); ?></time>
-				</div>
-				<?php endif;?>
-				<h2 class="wp-block-post-title">
-					<a href="<?php echo get_the_permalink(get_the_ID());?>"><?php echo get_the_title( get_the_ID() );?></a>
-				</h2>
-				<div class="wp-block-post-excerpt">
-					<p class="wp-block-post-excerpt__excerpt"><?php echo get_the_excerpt( get_the_ID() );?></p>
-					<p class="wp-block-post-excerpt__more-text">
-						<a class="wp-block-post-excerpt__more-link" href="<?php echo get_the_permalink( get_the_ID() );?>">続きを読む</a>
-					</p>
-				</div>
-				<?php if(!is_search()):?>
-				<div class="wp-block-columns">
-					<?php if($cat_terms):?>
-						<div class="wp-block-column" style="flex-basis:33.33%">
-							<div class="taxonomy-category is-cat wp-block-post-terms">
-								<?php foreach($cat_terms as $cat_term): ?>
-									<a href="<?php echo get_term_link( $cat_term->term_id );?>" rel="tag"><?php echo $cat_term->name;?></a>
-								<?php endforeach; ?>
-							</div>
+		<div class="is-layout-flow wp-block-column is-thumbnail" style="flex-basis:25%">
+			<?php echo $thumbnail;?>
+		</div>
+		<div class="is-layout-flow wp-block-column" style="flex-basis:75%">
+			<div class="is-layout-flow wp-block-group is-style-row-gap-small">
+				<div class="wp-block-group__inner-container">
+					<?php if(!is_search(  )):?>
+						<div class="wp-block-post-date">
+							<time datetime="<?php echo get_the_time("Y-m-d"); ?>"><?php echo get_the_time("Y.m.d"); ?></time>
 						</div>
 					<?php endif;?>
+					<h2 class="wp-block-post-title">
+						<a href="<?php echo get_the_permalink(get_the_ID());?>"><?php echo get_the_title( get_the_ID() );?></a>
+					</h2>
+					<div class="wp-block-post-excerpt">
+						<p class="wp-block-post-excerpt__excerpt"><?php echo get_the_excerpt( get_the_ID() );?></p>
+						<p class="wp-block-post-excerpt__more-text">
+							<a class="wp-block-post-excerpt__more-link" href="<?php echo get_the_permalink( get_the_ID() );?>">続きを読む</a>
+						</p>
+					</div>
+					<?php if(!is_search()):?>
+					<div class="wp-block-columns">
+						<?php if($cat_terms):?>
+							<div class="wp-block-column" style="flex-basis:33.33%">
+								<div class="taxonomy-category is-cat wp-block-post-terms">
+									<?php foreach($cat_terms as $cat_term): ?>
+										<a href="<?php echo get_term_link( $cat_term->term_id );?>" rel="tag"><?php echo $cat_term->name;?></a>
+									<?php endforeach; ?>
+								</div>
+							</div>
+						<?php endif;?>
+					</div>
+					<?php endif;?>
 				</div>
-				<?php endif;?>
-
 			</div>
 		</div>
 	</div>
